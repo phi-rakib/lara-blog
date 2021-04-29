3b000da705c654bdbf585afab00e1ead31d8047b
rakib <phi.rakib@gmail.com>
2021-04-29 23:35:59 +0600

    added repository service provider for binding all repositories to service container

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use App\Repositories\User\UserRepository;
+use App\Repositories\User\UserRepositoryInterface;
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    public $bindings = [
+        UserRepositoryInterface::class => UserRepository::class,
+    ];
+    
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
