0ff0067303c53ab30d7abcdf3dbc96e455d0d74d
rakib <phi.rakib@gmail.com>
2021-04-30 03:03:03 +0600

    added api resource route for post controller

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,24 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Response;
 
+Route::middleware('auth:sanctum')->group(function() {
+    Route::resource('posts', PostController::class);
+});
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/registration', [AuthController::class, 'registration']);
+
+
+
+Route::fallback(function () {
+    return response()->json(
+        [
+            'message' => 'Page Not Found. If error persists, contact administrator of the site',
+        ],
+        Response::HTTP_NOT_FOUND
+    );
+});
