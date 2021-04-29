80573ce4d537b8155610877b280508c2e43c496e
rakib <phi.rakib@gmail.com>
2021-04-30 02:59:29 +0600

    bind post repository to service container

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,14 +2,17 @@
 
 namespace App\Providers;
 
-use App\Repositories\User\UserRepository;
-use App\Repositories\User\UserRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\Post\PostRepository;
+use App\Repositories\User\UserRepository;
+use App\Repositories\Post\PostRepositoryInterface;
+use App\Repositories\User\UserRepositoryInterface;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
     public $bindings = [
         UserRepositoryInterface::class => UserRepository::class,
+        PostRepositoryInterface::class => PostRepository::class,
     ];
     
     /**
