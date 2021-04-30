0ed4d1fd96f1b6d90513eea1951ccd320189b637
rakib <phi.rakib@gmail.com>
2021-04-30 20:33:11 +0600

    create post by using user model relationship, now single and all post fetch  will get selected columns instead of all columns

--- app/Repositories/Post/PostRepository.php
+++ app/Repositories/Post/PostRepository.php
@@ -3,22 +3,33 @@
 namespace App\Repositories\Post;
 
 use App\Models\Post;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class PostRepository implements PostRepositoryInterface
 {
+    private function selectedColumns(): array
+    {
+        return ['id', 'title', 'body', 'user_id', 'created_at', 'updated_at'];
+    }
+
     public function get($id)
     {
-        return Post::findOrFail($id);
+        return Post::findOrFail($id, $this->selectedColumns());
     }
 
     public function all()
     {
-        return Post::all(['id', 'title', 'body', 'created_at']);
+        return Post::all($this->selectedColumns());
     }
 
     public function create($post)
     {
-        return Post::create($post);
+        /**
+         * @var User
+         */
+        $user = Auth::user();
+        return $user->posts()->create($post);
     }
 
     public function delete($id)
