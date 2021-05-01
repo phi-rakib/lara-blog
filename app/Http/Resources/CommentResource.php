429ac9f8e0e97f1e0e0b5a1b75b15c77d6c01d67
rakib <phi.rakib@gmail.com>
2021-05-01 21:57:16 +0600

    comment response

---
+++ app/Http/Resources/CommentResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Http\Resources\UserResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CommentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'post_id' => $this->post_id,
+            'user_id' => $this->user_id,
+            'body' => $this->body,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'user' => new UserResource($this->user),
+        ];
+    }
+}
