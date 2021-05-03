05d62a88be400c872024eae5cfd5d29d83fb0bbe
rakib <phi.rakib@gmail.com>
2021-05-03 23:42:59 +0600

    one to one polymorphic relation created

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -30,4 +30,9 @@ class Post extends Model
     {
         return $this->hasMany(Comment::class);
     }
+
+    public function image()
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -51,4 +51,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Comment::class);
     }
+
+    public function image()
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
 }
