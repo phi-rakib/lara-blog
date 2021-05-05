06c0ff620591c1c42570305d577a56f790060e63
rakib <phi.rakib@gmail.com>
2021-05-05 17:27:38 +0600

    added auth attemp functionality to auth repostiory

--- app/Repositories/Auth/AuthRepository.php
+++ app/Repositories/Auth/AuthRepository.php
@@ -5,13 +5,21 @@ namespace App\Repositories\Auth;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
-class AuthRepository implements AuthRepositoryInterface {
+class AuthRepository implements AuthRepositoryInterface
+{
 
-  public function getAuthUser() : User {
-    return Auth::user();
-  }
+    public function getAuthUser(): User
+    {
+        return Auth::user();
+    }
 
-  public function getAuthId() : int {
-    return Auth::id();
-  }
+    public function getAuthId(): int
+    {
+        return Auth::id();
+    }
+
+    public function authAttempt($user): bool
+    {
+        return Auth::attempt($user);
+    }
 }
--- app/Repositories/Auth/AuthRepositoryInterface.php
+++ app/Repositories/Auth/AuthRepositoryInterface.php
@@ -7,4 +7,5 @@ use App\Models\User;
 interface AuthRepositoryInterface {
   public function getAuthUser() : User;
   public function getAuthId() : int;
+  public function authAttempt($user) : bool;
 }
