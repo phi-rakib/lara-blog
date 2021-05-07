7fc1d5eba373487e0dd63cef58248e021a92a5a0
rakib <phi.rakib@gmail.com>
2021-05-07 22:47:19 +0600

    binded profileRepositoryInterface to service container

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -8,6 +8,8 @@ use App\Repositories\Comment\CommentRepository;
 use App\Repositories\Comment\CommentRepositoryInterface;
 use App\Repositories\Post\PostRepository;
 use App\Repositories\Post\PostRepositoryInterface;
+use App\Repositories\Profile\ProfileRepository;
+use App\Repositories\Profile\ProfileRepositoryInterface;
 use App\Repositories\User\UserRepository;
 use App\Repositories\User\UserRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
@@ -18,6 +20,7 @@ class RepositoryServiceProvider extends ServiceProvider
         UserRepositoryInterface::class => UserRepository::class,
         PostRepositoryInterface::class => PostRepository::class,
         CommentRepositoryInterface::class => CommentRepository::class,
+        ProfileRepositoryInterface::class => ProfileRepository::class,
     ];
 
     public $singletons = [
