5b71a368f742ef1b1a6cfd839c1303812f9daa9c
rakib <phi.rakib@gmail.com>
2021-05-07 22:42:42 +0600

    added one to one relation with profile

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,9 @@ class User extends Authenticatable
     {
         return $this->morphOne(Image::class, 'imageable');
     }
+
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
+    }
 }
