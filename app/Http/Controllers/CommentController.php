a1dbfaed18efce5043dc503379d617ba9d968ec1
rakib <phi.rakib@gmail.com>
2023-05-29 12:16:26 +0600

    used facade instead of repository in comment ctrl

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,72 +3,44 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CommentRequest;
-use App\Models\Comment;
-use App\Repositories\Comment\CommentRepositoryInterface;
+use App\Facades\CommentFacade;
+use Illuminate\Support\Facades\Log;
 
 class CommentController extends ApiController
 {
-    private $commentRepository;
 
-    public function __construct(CommentRepositoryInterface $commentRepository)
+    public function __construct()
     {
-        $this->commentRepository = $commentRepository;
         $this->middleware('auth:sanctum')->only(['store', 'update', 'destroy']);
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        return $this->commentRepository->getAllComments();
+        return CommentFacade::getAll();
     }
 
     public function show($commentId)
     {
-        return $this->commentRepository->commentsById($commentId, ['user:id,name']);
+        return CommentFacade::getById($commentId);
     }
 
     public function commentsByPostId($postId)
     {
-        return $this->commentRepository->commentsByPostId($postId);
+        return CommentFacade::getByPostId($postId);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param int $post
-     * @return \Illuminate\Http\Response
-     */
     public function store(CommentRequest $request)
     {
-        return $this->commentRepository->create($request->all());
+        return CommentFacade::create($request->all());
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(CommentRequest $request, $id)
     {
-        $this->commentRepository->update($id, $request->all());
+        CommentFacade::update($id, $request->all());
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param CommentRequest $request
-     * @param int $id
-     * @return void
-     */
     public function destroy($id)
     {
-        $this->commentRepository->delete($id);
+        CommentFacade::delete($id);
     }
 }
